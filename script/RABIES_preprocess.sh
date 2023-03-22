d1aba8be4419ce4f79e58f2ce6ddb741676d5230
Marie Galteau <marie.galteau@ru.nl>
2023-03-22 18:29:55 +0100

    add 3rd argument for correction

--- script/RABIES_preprocess.sh
+++ script/RABIES_preprocess.sh
@@ -25,13 +25,15 @@ csf=/template/SIGMA_Rat_Anatomical_Imaging/SIGMA_Rat_Anatomical_InVivo_Template/
 atlas=/template/SIGMA_Rat_Brain_Atlases/SIGMA_Anatomical_Atlas/SIGMA_Anatomical_Brain_Atlas_rs.nii
 roi=/groupshare/traaffneu/preclinimg/template/roi/
 
-#Define input/output folders for subject sub-0101000 ses-1
+#Define input/output folders for subject sub-020XXXX ses-1
 subj_number=$1
-TR=$2 
+TR=$2
+correction_arg=$3
+
 orig_folder=/project/4180000.19/multirat_stim/rabies/bids/sub-${subj_number}/ses-1
 template_folder=/groupshare/traaffneu/preclinimg/templates/SIGMA_Wistar_Rat_Brain_TemplatesAndAtlases_Version1.1
 bids_folder=/project/4180000.19/multirat_stim/rabies/bids/sub-${subj_number}_ses-1
-preprocess_folder=/project/4180000.19/multirat_stim/rabies/preprocess/sub-${subj_number}_ses-1
+preprocess_folder=/project/4180000.19/multirat_stim/rabies_test/preprocess/sub-${subj_number}_ses-1
 
 #Clean and make directories
 rm -rf $bids_folder
@@ -51,12 +53,14 @@ singularity run -B ${template_folder}:/template -B ${bids_folder}:/rabies_in:ro 
 --CSF_mask ${csf} \
 --vascular_mask ${csf} \
 --labels ${atlas} \
-# --anat_robust_inho_cor='apply'='true','masking'='true','brain_extraction'='true','template_registration'='SyN' \
-# --bold_robust_inho_cor='apply'='true','masking'='true','brain_extraction'='true','template_registration'='SyN' \
-# --commonspace_reg='masking'='false','brain_extraction'='true','template_registration'='SyN','fast_commonspace'='true' \
-# --commonspace_resampling 0.3x0.3x0.3 \
-# --anat_autobox \
-# --bold_autobox \
+${correction_arg} \
 
-#--anat_inho_cor='method'='N4_reg','otsu_thresh'='2','multiotsu'='false' \
-#--bold_inho_cor='method'='N4_reg','otsu_thresh'='2','multiotsu'='false' \
+    # --anat_robust_inho_cor='apply'='true','masking'='true','brain_extraction'='true','template_registration'='SyN' \
+    # --bold_robust_inho_cor='apply'='true','masking'='true','brain_extraction'='true','template_registration'='SyN' \
+    # --commonspace_reg='masking'='false','brain_extraction'='true','template_registration'='SyN','fast_commonspace'='true' \
+    # --commonspace_resampling 0.3x0.3x0.3 \
+    # --anat_autobox \
+    # --bold_autobox \
+
+    #--anat_inho_cor='method'='N4_reg','otsu_thresh'='2','multiotsu'='false' \
+    #--bold_inho_cor='method'='N4_reg','otsu_thresh'='2','multiotsu'='false' \
