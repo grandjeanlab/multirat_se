a331d29360e3cac615b95653b3e5237a24bcfca3
Marie Galteau <marie.galteau@ru.nl>
2023-04-13 17:36:02 +0200

    updated for yulab, output datasets 18, 19, 20

--- asset/convert_bruker.sh
+++ asset/convert_bruker.sh
@@ -5,7 +5,7 @@
 #add to .bashrc --> PATH=$PATH:/groupshare/traaffneu/preclinimg/software/Bru2
 
 #qsub -l 'procs=1,mem=24gb,walltime=12:00:00' -I 
-#cd /home/traaffneu/margal/code/multirat_se/scripts/
+#cd /home/traaffneu/margal/code/multirat_se/asset
 # ./convert_bruker.sh
 
 
@@ -13,22 +13,22 @@
 module load afni
 root_dir='/project/4180000.19/multirat_stim/to_convert/test_marie'
 
-cd $root_dir/raw_lupe/data                       #directory of data to be converted into bids format
+cd $root_dir/raw_yulab                         #directory of data to be converted into bids format
 
 output_dir=$root_dir'/export'                  #create the variable output_dir to be put in folder export  
 mkdir -p $output_dir 
 
-dataset_name='Lupe'                           #name dataset received 
+dataset_name='YuLab'                           #name dataset received 
 
 ds_type=02 
-ds_id=013                                    #because already 12 datasets aquired (see, /project/4180000.19/multirat_stim/bids/)
-id=0    
+ds_id=020                                      #because already 12 datasets aquired (see, /project/4180000.19/multirat_stim/bids/)
+id=0  
 
 # --- Convert to BIDS ---- 
 
 ls . | while read line                    #line is a variable, while read all folders (=line) 
 do                                        #start loop 
-cd $root_dir/raw_lupe/data/$line         #go to directory of each line/folder
+cd $root_dir/raw_yulab/$line              #go to directory of each line/folder
 
 sub=$ds_type$ds_id'0'$id                  #define subject ID
 
@@ -43,16 +43,19 @@ anat_name='sub-'$sub'_ses-'$ses'_T2w.nii.gz'                                    
 func_name='sub-'$sub'_ses-'$ses'_run-1_bold.nii.gz'
 
 #anat
-Bru2 -a -z -o tmp anat/pdata/1/                                                             #convert create tmp folder into anat/acqp directory -> convert
-3dresample -inset tmp.nii.gz[0] -prefix  $output_sub_dir'/anat/'$anat_name -orient LPI         #change scan orientation, save into output directory 
+Bru2 -a -z -o tmp anat/pdata/1/                                                            #convert create tmp folder into anat/acqp directory -> convert
+3dWarp -deoblique tmp.nii.gz
+3dresample -inset tmp.nii.gz -prefix  $output_sub_dir'/anat/'$anat_name -orient LPI         #change scan orientation, save into output directory 
 rm tmp.nii.gz
+3drefit -cmass center $output_sub_dir'/anat/'$anat_name                                     # so that the images are properly centered
+
 
 #func
-func_name='sub-'$sub'_ses-'$ses'_run-1-1_bold.nii.gz'
-
-Bru2 -z -a -o tmp func/pdata/1/                                                           #convert data in folder 6 into BIDS format
+Bru2 -z -a -o tmp func_2/pdata/1/                                                           #convert data in folder 6 into BIDS format
+3dWarp -deoblique tmp.nii.gz
 3dresample -inset tmp.nii.gz -prefix  $output_sub_dir'/func/'$func_name -orient LPI        #Change scan orientation, save into output directory 
 rm tmp.nii.gz
+3drefit -cmass center $output_sub_dir'/func/'$func_name
 
 #func_name='sub-'$sub'_ses-'$ses'_run-1-1000_bold.nii.gz'
 #Bru2 -z -a -o tmp_1-1000 func_1/pdata/1000/                                                                        #convert data in folder 6 into BIDS format
