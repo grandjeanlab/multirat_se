0f645c3c13ab6cd020671c464df779117f2e34ad
Marie Galteau <marie.galteau@ru.nl>
2023-07-11 16:05:44 +0200

    dataset 10

--- asset/convert_bruker.sh
+++ asset/convert_bruker.sh
@@ -13,22 +13,22 @@
 module load afni
 root_dir='/project/4180000.19/multirat_stim/scratch/to_convert/test_marie'
 
-cd $root_dir/raw_hess/heat_stim/data/                        #directory of data to be converted into bids format
+cd $root_dir/raw_sumiyoshi/data/                        #directory of data to be converted into bids format
 
 output_dir=$root_dir'/export'                  #create the variable output_dir to be put in folder export  
 mkdir -p $output_dir 
 
-dataset_name='Hess'                           #name dataset received 
+dataset_name='Sumiyoshi'                           #name dataset received 
 
 ds_type=02 
-ds_id=014                                      #because already 12 datasets aquired (see, /project/4180000.19/multirat_stim/bids/)
+ds_id=010                                      #because already 12 datasets aquired (see, /project/4180000.19/multirat_stim/bids/)
 id=0  
 
 # --- Convert to BIDS ---- 
 
 ls . | while read line                    #line is a variable, while read all folders (=line) 
 do                                        #start loop 
-cd $root_dir/raw_hess/heat_stim/data/$line              #go to directory of each line/folder
+cd $root_dir/raw_sumiyoshi/data/$line              #go to directory of each line/folder
 
 sub=$ds_type$ds_id'0'$id                  #define subject ID
 
@@ -39,18 +39,33 @@ output_sub_dir=$output_dir'/sub-'$sub'/ses-'$ses                                
 mkdir -p $output_sub_dir'/anat'                                                           #withing the directory output, create 2 directories: anat et func
 mkdir -p $output_sub_dir'/func'
 
-anat_name='sub-'$sub'_ses-'$ses'_T2w.nii.gz'                                               #define 2 variables to name file later, based on subject and session ID
-func_name='sub-'$sub'_ses-'$ses'_run-1_bold.nii.gz'
+anat_name='sub-'$sub'_ses-'$ses'_T2w.nii'                                               #define 2 variables to name file later, based on subject and session ID
+func_name='sub-'$sub'_ses-'$ses'_run-1_bold.nii'
 
-#anat
-Bru2 -a -z -o tmp anat/pdata/1/                                                            #convert create tmp folder into anat/acqp directory -> convert
-3dresample -inset tmp.nii.gz -prefix  $output_sub_dir'/anat/'$anat_name -orient LPI         #change scan orientation, save into output directory 
-rm tmp.nii.gz
 
-#func
-Bru2 -z -a -o tmp func/pdata/1/                                                           #convert data in folder 6 into BIDS format
-3dresample -inset tmp.nii.gz -prefix  $output_sub_dir'/func/'$func_name -orient LPI        #Change scan orientation, save into output directory 
-rm tmp.nii.gz
+# anat   
+cp $root_dir'/raw_sumiyoshi/data/'$line'/anat/'$line'_anat.nii' $output_sub_dir'/anat/'tmp_$anat_name         # copy the anat file into the output directory and rename it accordingly
+fslorient -setsform 1 0 0 0 0 0 1 0 0 1 0 0 0 0 0 1 $output_sub_dir'/anat/'tmp_$anat_name                     # correct the labelled of the orientation, in this case to get posterior to anterior and superior to inferior
+fslreorient2std $output_sub_dir'/anat/'tmp_$anat_name  $output_sub_dir'/anat/'$anat_name 
+rm $output_sub_dir'/anat/'tmp_$anat_name
+
+
+# func
+cp $root_dir'/raw_sumiyoshi/data/'$line'/func/'$line'_func.nii' $output_sub_dir'/func/'tmp_$func_name         # copy the anat file into the output directory and rename it accordingly
+fslorient -setsform 1 0 0 0 0 0 1 0 0 1 0 0 0 0 0 1  $output_sub_dir'/func/'tmp_$func_name                    # correct the labelled of the orientation, in this case to get posterior to anterior and superior to inferior
+fslreorient2std $output_sub_dir'/func/'tmp_$func_name  $output_sub_dir'/func/'$func_name                      #Change scan orientation to match stadard one
+rm $output_sub_dir'/func/'tmp_$func_name
+
+
+# #anat
+# Bru2 -a -z -o tmp anat/                                                                     #convert create tmp folder into anat/acqp directory -> convert
+# 3dresample -inset tmp.nii.gz -prefix  $output_sub_dir'/anat/'$anat_name -orient LPI         #change scan orientation, save into output directory 
+# rm tmp.nii.gz
+
+# #func
+# Bru2 -z -a -o tmp func/                                                                    #convert data in folder 6 into BIDS format
+# 3dresample -inset tmp.nii.gz -prefix  $output_sub_dir'/func/'$func_name -orient LPI        #Change scan orientation, save into output directory 
+# rm tmp.nii.gz
 
 id=$((id + 1))                  #id number increases by 1
 
