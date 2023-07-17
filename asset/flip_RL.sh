cd80b5611682ca92886dbececb9425a904f78d20
Marie Galteau <marie.galteau@ru.nl>
2023-07-17 10:51:37 +0200

    flip RL for dataset 3 5, 7, 11, 12

---
+++ asset/flip_RL.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# cd /home/traaffneu/margal/code/multirat_se/asset
+# ./flip_RL.sh
+
+
+cd /project/4180000.19/multirat_stim/scratch/rabies_test/flip/
+
+nifti_file="sub-0201202_ses-1_run-1_bold_combined.nii.gz"
+
+fslhd $nifti_file
+
+fslswapdim $nifti_file -x y z flipped/flipped_$nifti_file
+
+fslhd $nifti_file
